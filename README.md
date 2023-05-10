f010025d4fd9c4497470f14bc7ea2bda668f28ae
UP900420 <UP900420@myport.ac.uk>
2023-05-10 17:46:39 +0100

    Ammending ESLint errors with ready.js

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # DiscordRoleBot
 akwfpfkaaf
 dwadwd
+
+
+# User Instructions:
+automatically on user joining server OR manually ran through command
+bot messages user asking for UP number
+user gives bot UP number (7 digits)
+bot compares up number given with up number on csv
+bot gives roles for year + course
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,28 +1,32 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable require-await */
+
 module.exports = {
-    name: 'ready',
-    once: true,
-    async execute(client) {
-        console.log('Ready!');
+  name: 'ready',
+  once: true,
+  async execute(client) {
+    console.log('Ready!');
 
-        async function pickPresence () {
-            const option = Math.floor(Math.random() * statusArray.length);
+    async function pickPresence() {
+      const option = Math.floor(Math.random() * statusArray.length);
 
-            try {
-                await client.user.setPresence({
-                    activities: [
-                        {
-                            name: statusArray[option].content,
-                            type: statusArray[option].type,
+      try {
+        await client.user.setPresence({
+          activities: [
+            {
+              name: statusArray[option].content,
+              type: statusArray[option].type,
 
-                        },
-                    
-                    ],
+            },
 
-                    status: statusArray[option].status
-                })
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    },
+          ],
+
+          status: statusArray[option].status,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
 };
