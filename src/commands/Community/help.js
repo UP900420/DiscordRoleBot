407ffd2c90118bdf18406d9d24235dc894e50148
UP900420 <UP900420@myport.ac.uk>
2023-05-10 19:48:16 +0100

    Disabling stuff for ESLint errors for now

--- src/commands/Community/help.js
+++ src/commands/Community/help.js
@@ -1,23 +1,24 @@
-const { SlashCommandBuilder } = require(`@discordjs/builders`);
-const { PermissionsBitField, ButtonStyle, ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require(`discord.js`);
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { PermissionsBitField, ButtonStyle, ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
+  data: new SlashCommandBuilder()
     .setName('help')
     .setDescription('This is the help command!'),
-    async execute (interaction, client) {
-
-        const embed = new EmbedBuilder()
-        .setColor("Blue")
-        .setTitle(`Reaction Roles`)
-        .setDescription(`**Welcome to the Reaction Role Bot**
+  async execute(interaction, client) {
+    const embed = new EmbedBuilder()
+      .setColor('Blue')
+      .setTitle('Reaction Roles')
+      .setDescription(`**Welcome to the Reaction Role Bot**
         \n This bot was built to assist discord servers in moderating their roles using multiple methods.
         \n __Bot commands:__
         \n - **/help** - **__Help Command__** - This displays instructions of how to utilise the bots commands within the server.
         \n - **/buttonrole** - **__Button Based Role Command__** - To use this command simply enter the slash command and enter what roles you want underneath the message.
-        \n - **/reactionrole** - **__Reaction Based Role Command__** - To use this command simply enter....`)
+        \n - **/reactionrole** - **__Reaction Based Role Command__** - To use this command simply enter....`);
 
-        await interaction.reply({ embeds: [embed]});
-    }
+    await interaction.reply({ embeds: [embed] });
+  },
 
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection } = require(`discord.js`);
+/* eslint-disable require-await */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection } = require('discord.js');
 const fs = require('fs');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] }); 
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.commands = new Collection();
 
 require('dotenv').config();
 
-const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
-const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
-const commandFolders = fs.readdirSync("./src/commands");
+const functions = fs.readdirSync('./src/functions').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+const commandFolders = fs.readdirSync('./src/commands');
 
 (async () => {
-    for (file of functions) {
-        require(`./functions/${file}`)(client);
-    }
-    client.handleEvents(eventFiles, "./src/events");
-    client.handleCommands(commandFolders, "./src/commands");
-    client.login(process.env.token)
+  for (file of functions) {
+    require(`./functions/${file}`)(client);
+  }
+  client.handleEvents(eventFiles, './src/events');
+  client.handleCommands(commandFolders, './src/commands');
+  client.login(process.env.token);
 })();
-
-
