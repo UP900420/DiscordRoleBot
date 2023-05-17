7112761938955475cc5d886d7528247fa84cf71f
UP900420 <UP900420@myport.ac.uk>
2023-05-17 11:22:24 +0100

    Making automated system work with entry of UP numbers with csv file.

--- src/index.js
+++ src/index.js
@@ -32,34 +32,44 @@ fs.createReadStream('./src/csv/studentdata.csv')
 
 // server join/leave logging
 client.on('guildMemberAdd', guildMember => {
-    guildMember.send(`Hello, ${guildMember.user} welcome to the University Discord Server, please post your UP number like this: "UP1234567" in the available text channel to get your roles and set up in the server.`);
+    // guildMember.send(`Hello, ${guildMember.user} welcome to the University Discord Server, please post your UP number like this: "UP1234567" in the available text channel to get your roles and set up in the server.`);
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'norole');
     
     guildMember.roles.add(welcomeRole);
-    console.log("someone joined")
+    
+    client.channels.cache.get('1106731042007691335').send(`Welcome ${guildMember.user}, please enter your UP number into this channel to be allocated your roles.`);
+
 });
 
 // active message listener in the "no role channel to give roles to new users."
 client.on('messageCreate', async (message) => {
-  if(message.author.bot) return;
   if(message.channelId != 1106731042007691335) return;
   for (var i = 0; i < student.length; i++) { 
-    // console.log(student[i].upnumber); TESTING.
+    console.log(student[i].upnumber);
     if (message.content === student[i].upnumber) {
       message.author.send(`Hello, ${student[i].firstname}`);
-      message.author.send(`You have been given the roles: Year ${student[i].year} and ${student[i].course} course`)
+      message.author.send(`You have been given the roles: Year ${student[i].year} and ${student[i].course} course and access to the co-existing channels`)
+      message.author.send(`You also have been given the roles: member to access the public text channels`)
+      const person = message.member;
+      console.log(`${student[i].year}`)
+      let yearRole = person.guild.roles.cache.find(role => role.name === `Year ${student[i].year}`);
+      console.log(`${student[i].course}`)
+      let courseRole = person.guild.roles.cache.find(role => role.name === `${student[i].course}`);
+      person.roles.add(yearRole);
+      person.roles.add(courseRole);
+      let memberRole = person.guild.roles.cache.find(role => role.name === `member`);
+      person.roles.add(memberRole);
+      let welcomeRole = person.guild.roles.cache.find(role => role.name === 'norole');
+      person.roles.remove(welcomeRole);
+      person.setNickname(`${student[i].firstname} ${student[i].lastname.charAt(0)} | ${student[i].upnumber}`);
+      break;
+      
+      
       // set this up to give roles and nickname.
       // user.setNickname(`{${student[i].firstName} +${student[i].lastName} | ${student[i].upnumber}}`);
-      return;
     }
   }
 });
-
-
-
-
-
-
 
 
 
