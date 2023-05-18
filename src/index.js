6439bbac05a522b923fe012691786f2aa8f47dc2
UP900420 <UP900420@myport.ac.uk>
2023-05-18 22:54:17 +0100

    Added more lines to CSV for stress testing

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ fs.createReadStream('./src/csv/studentdata.csv')
   .pipe(csv.parse({ headers: true }))
   .on('error', error => console.error(error))
   .on('data', row => student.push(row))
-  .on('end', () => console.log("Pipe completed"));
+  .on('end', () => console.log("CSV Data imported"));
 
 (async () => {
   for (file of functions) {
@@ -32,20 +32,20 @@ fs.createReadStream('./src/csv/studentdata.csv')
 
 // server join/leave logging
 client.on('guildMemberAdd', guildMember => {
-    // guildMember.send(`Hello, ${guildMember.user} welcome to the University Discord Server, please post your UP number like this: "UP1234567" in the available text channel to get your roles and set up in the server.`);
+    guildMember.send(`Hello, ${guildMember.user} welcome to the University Discord Server, please post your UP number like this: "UP1234567" in the available text channel to get your roles and set up in the server.`);
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'norole');
     
     guildMember.roles.add(welcomeRole);
     
-    client.channels.cache.get('1106731042007691335').send(`Welcome ${guildMember.user}, please enter your UP number into this channel to be allocated your roles.`);
+    client.channels.cache.get('1108863268883279972').send(`Welcome ${guildMember.user}, please enter your UP number into this channel to be allocated your roles.`), 1000;
 
 });
 
 // active message listener in the "no role channel to give roles to new users."
 client.on('messageCreate', async (message) => {
-  if(message.channelId != 1106731042007691335) return;
+  if(message.channelId != 1108863268883279972) return;
   for (var i = 0; i < student.length; i++) { 
-    console.log(student[i].upnumber);
+    
     const person = message.member;
     if (message.content === student[i].upnumber) {
       message.author.send(`Hello, ${student[i].firstname}`);
@@ -62,14 +62,27 @@ client.on('messageCreate', async (message) => {
       let welcomeRole = person.guild.roles.cache.find(role => role.name === 'norole');
       person.roles.remove(welcomeRole);
       person.setNickname(`${student[i].firstname} ${student[i].lastname.charAt(0)} | ${student[i].upnumber}`);
+      console.log(`CSV ID:${[i]}`);
       break;
     }
       // adjustable password to give whatever the admin role is for teachers/lecturers etc.
     else if (message.content === "specific admin password") {
       let adminRole = person.guild.roles.cache.find(role => role.name === 'admin');
       person.roles.add(adminRole);
-    }   
+    }
   }
+
+  if (message.author.bot) {
+    setTimeout(() => { message.delete();}, 120000);
+    return
+    
+  }
+  else {
+    setTimeout(() => { message.delete();}, 20000);
+    return;
+  }
+  
+  
 });
 
 
